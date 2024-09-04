720a3d78774d234ead4a39e1ccb4e826c08b20a4
Mark Santoro <mark_santoro@comcast.net>
2024-09-03 19:29:49 -0700

    Initial Commit

---
+++ CircularSwiftUI/CircularSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircularSwiftUIApp.swift
+//  CircularSwiftUI
+//
+//  Created by Mark Santoro on 9/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CircularSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
